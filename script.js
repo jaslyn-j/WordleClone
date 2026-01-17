191b5eb1e5f4318b7f70982535a91a2dad4a3e76
jaslyn-j <jaslynjacob026@gmail.com>
2026-01-17 19:07:47 +0530

    basic structure

---
+++ script.js
@@ -0,0 +1,91 @@
+createSquares();
+const keys=document.querySelectorAll('.keyboard-row button');
+let guessedWords=[[]];
+let availableSpace=1;
+let word="fiery";
+let guessedWordCount=0;
+
+function createSquares(){
+    const gameBoard=document.getElementById("board");
+    for(let index=0; index<30; index++){
+        let square=document.createElement("div");
+        square.classList.add("square");
+        square.classList.add("animate__animated");
+        square.setAttribute("id", index+1);
+        gameBoard.appendChild(square);
+    }
+}
+
+function getCurrentWordArr(){
+    const numberOfGuessedWords=guessedWords.length;
+    return guessedWords[numberOfGuessedWords-1];
+}
+
+function updateGuessedWords(letter){
+    const currentWordArr=getCurrentWordArr();
+    if(currentWordArr && currentWordArr.length<5){
+        currentWordArr.push(letter);
+
+        const availableSpaceEl=document.getElementById(String(availableSpace));
+        availableSpace+=1;
+        availableSpaceEl.textContent=letter;
+    }
+}
+function getTileColor(letter, index){
+    const isCorrectLetter=word.includes(letter);
+    if(!isCorrectLetter){
+        return "rgb(58,58,60)";
+    }
+    const letterInThatPosition=word.charAt(index);
+    const isCorrectPosition= (letter===letterInThatPosition);
+    if(isCorrectPosition){
+        return "rgb(83, 141, 78)";
+    }return "rgb(181,159, 59)";
+}
+
+function handleSubmitWord(){
+    const currentWordArr=getCurrentWordArr();
+    if(currentWordArr.length !==5){
+        window.alert("Not enough letters");
+    }
+    const currentWord=currentWordArr.join("");
+    const firstLetterId=guessedWordCount*5 + 1;
+
+    const interval=200;
+    currentWordArr.forEach((letter, index) => {
+        setTimeout(()=>{
+            const tileColor=getTileColor(letter, index);
+
+            const letterId=firstLetterId+index;
+            const letterEl=document.getElementById(letterId);
+            letterEl.classList.add("animate__flipInX");
+            letterEl.style=`background-color:${tileColor}; border-color:${tileColor}`;
+        }, interval*index);
+
+    })
+    guessedWordCount+=1;
+
+    if(currentWord===word){
+        window.alert("Congratulations!")
+    }
+    if(guessedWords.length===6){
+        window.alert(`${word}`);
+    }
+    guessedWords.push([]);
+}
+
+
+for (let i= 0; i<keys.length; i++){
+    keys[i].onclick=({target})=>{
+        const letter=target.getAttribute("data-key");
+
+        if(letter==='enter'){
+            handleSubmitWord();
+            return;
+        }
+
+        updateGuessedWords(letter);
+
+    }
+    
+}
